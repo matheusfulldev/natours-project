8051d999a6d8e832a0df66a79f8cc404a6ddd220
Matheus Ramos <matheusfulldev@gmail.com>
2022-09-18 01:27:18 -0300

    update

--- script/script.js
+++ script/script.js
@@ -2,3 +2,8 @@ const $btnAnimated = document.querySelector(".btn--animated");
 $btnAnimated.addEventListener("animationend", function () {
   $btnAnimated.classList.remove("btn--animated");
 });
+
+const $logoAnimated = document.querySelector(".header__logo--animated");
+  $logoAnimated.addEventListener("animationend", function () {
+  $logoAnimated.classList.remove("header__logo--animated");
+});
